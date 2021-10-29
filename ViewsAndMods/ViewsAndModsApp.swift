f10720afa109f51cd026361bdb5667ac9a540c19
Derek Velzy <dmvelzy@gmail.com>
2021-10-29 07:21:58 -0500

    setup project

---
+++ ViewsAndMods/ViewsAndModsApp.swift
@@ -0,0 +1,17 @@
+//
+//  ViewsAndModsApp.swift
+//  ViewsAndMods
+//
+//  Created by Derek Velzy on 10/29/21.
+//
+
+import SwiftUI
+
+@main
+struct ViewsAndModsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
